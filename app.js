2a7d97c461585f8bb5903dfc91373e7a979c2409
francisca-rivera <franda.chi@gmail.com>
2022-07-18 23:30:26 -0400

    Saque un link index2.html porque no lo voy a usar, agregue comentarios en JS, un h3 en html y un footer. Al footer y al h3 les di un poco de estilo con css

--- app.js
+++ app.js
@@ -9,55 +9,68 @@ function inicio() {
     function () {
       this.value = this.value.toUpperCase(); //para mayúsculas.
     },
-    true
+    true //bubbling phase
   );
   //click en el botón cifrar, llamará a una función.
   document.getElementById('encode').addEventListener(
     'click',
     function () {
-      let texto = document.getElementById('mensaje').value;
-      let desplazamiento = document.getElementById('desplazamiento').value;
-      document.getElementById('mensaje2').value = encode(texto, desplazamiento); //para escribir en el input encode
+      let texto = document.getElementById('mensaje').value; //para sacar el valor del texto de mensaje.
+      let desplazamiento = document.getElementById('desplazamiento').value; //para sacar el valor de desplazamiento.
+      document.getElementById('mensaje2').value = encode2(
+        texto,
+        desplazamiento
+      ); //para escribir mensaje en el input encode
     },
     true
   );
+  //igual que la función encode.
   document.getElementById('decode').addEventListener(
     'click',
     function () {
       let texto = document.getElementById('mensaje').value;
       let desplazamiento = document.getElementById('desplazamiento').value;
-      document.getElementById('mensaje2').value = decode(texto, desplazamiento); //para escribir en el input decode
+      document.getElementById('mensaje2').value = decode(texto, desplazamiento); //para escribir mensaje en el input decode
     },
     true
   );
 }
-
+//función encode
 function encode(texto, desplazamiento) {
-  let resultado = '';
-  const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
+  let resultado = ''; //funcion que devuelve el resultado mensaje2 al input mensaje cifrado/desifrado.
+  const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; //cadena con el abecedario
+  //operación módulo
   desplazamiento = ((desplazamiento % 26) + 26) % 26;
-
+  //para recorrer la cadena e ir rotando cada letra
   if (texto) {
+    //for es un ciclo
     for (let i = 0; i < texto.length; i++) {
+      //si encuentra un caracter o un espacio, lo devolverá tal cual
       if (letras.indexOf(texto[i]) != -1) {
+        //para guardar la posicion de una letra dentro de la cadena
         let posicion = (letras.indexOf(texto[i]) + desplazamiento) % 26;
+        //aquí se guardará cada letra nueva rotada, correspodiente a cada una de las posiciones.
         resultado += letras[posicion];
+        //retornará caracteres y numeros en caso de haberlos
       } else resultado += texto[i];
     }
   }
   return resultado;
 }
-
-function decode2(texto, desplazamiento) {
-  if (!texto) return '';
+//funcion encode
+function encode2(texto, desplazamiento) {
+  if (!texto) return ''; //función vacía
   const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   desplazamiento = ((desplazamiento % 26) + 26) % 26;
+  //replace le pasamos la cadena y le indicamos que queremos hacer con cada letra
   return texto.replace(
     /[A-Z]/gi,
+    /*c=> recorerá la cadena 1 a 1 las letas de la cadena A-Z dentro de texto
+    para cada una de las letras comprobar su posición y se le añade el desplazamiento*/
     (c) => letras[(letras.indexOf(c) + desplazamiento) % 26]
   );
 }
+//funcion decode
 function decode(texto, desplazamiento) {
   if (!texto) return '';
   const letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
